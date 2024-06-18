9a1c267631b6b29c23b1913239605326440aef74
desto4q <desto4q@gmail.com>
2024-06-18 16:13:34 +0100

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
             <div className=' fixed bg-neutral-800 w-full bottom-0 h-full left-0 -z-10 flex flex-col'>
                 <div className='mt-auto'>
                     <div className='mx-auto max-w-xl flex  justify-center flex-col items-center'>
-                        <h1 className='text-3xl'>Stream Your Favorite Anime</h1>
+                        <h1 className='text-3xl text-center'>Stream Your Favorite Anime</h1>
                         <p className='text-neutral-300 text-lg capitalize'>download Gview now</p>
                         <div className='flex flex-col sm:flex-row items-center gap-6 py-6'>
                             <Button className='h-max gap-2'>
