bfd7dc1683fa012cec56363e994403ad8e214893
desto4q <desto4q@gmail.com>
2024-06-18 16:10:27 +0100

    typescript is ass

--- src/components/IconCont.tsx
+++ src/components/IconCont.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import  { ReactNode } from 'react'
 
 function IconCont({ children, ClassName }: { children?: ReactNode, ClassName?: string }) {
     return (
